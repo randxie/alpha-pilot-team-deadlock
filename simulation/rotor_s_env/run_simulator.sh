e59580eb2b95eb64c3879be2c9cdd8fe2161c866
Rand Xie <randxiexyy29@gmail.com>
2019-01-13 17:08:28 -0800

    Update run_simulator.sh

--- simulation/rotor_s_env/run_simulator.sh
+++ simulation/rotor_s_env/run_simulator.sh
@@ -1,2 +1,6 @@
 roslaunch gazebo_ros empty_world.launch
 roslaunch rotors_gazebo mav_hovering_example.launch
+
+# enter the running container
+docker ps
+docker exec -it <CONTAINER ID> /bin/bash # please replace the container id with yours
