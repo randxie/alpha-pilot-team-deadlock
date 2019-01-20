62895afdff074c9d95b360ef5aeb2101fbe776f5
randxie <randxiexyy29@gmail.com>
2019-01-19 19:35:30 -0800

    update train config

--- vision/gate_detection/src/submit_local_training.sh
+++ vision/gate_detection/src/submit_local_training.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #python object_detection/model_main.py --model_dir=../results/ssd-mobilenet-v1-fpn --pipeline_config_path=configs/ssd-mobilenet-v1-fpn/train.config --num_train_steps=80000 --sample_1_of_n_eval_examples=1 --alsologtostderr
 
-python object_detection/model_main.py --model_dir=../results/ssd-resnet50-v1-fpn --pipeline_config_path=configs/ssd-resnet50-v1-fpn/train.config --num_train_steps=80000 --sample_1_of_n_eval_examples=10 --alsologtostderr
+python object_detection/model_main.py --model_dir=../results/ssd-resnet50-v1-fpn --pipeline_config_path=configs/ssd-resnet50-v1-fpn/train.config --num_train_steps=80000 --sample_1_of_n_eval_examples=1 --alsologtostderr
 
 
 # get tensorboard
