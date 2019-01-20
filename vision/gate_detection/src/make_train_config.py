31d176e2d9a7663b35a5c7a26a576acd97329cce
randxie <randxiexyy29@gmail.com>
2019-01-19 19:28:54 -0800

    add ssd-resnet50

--- vision/gate_detection/src/make_train_config.py
+++ vision/gate_detection/src/make_train_config.py
@@ -2,10 +2,12 @@ import os
 from .common import PROJECT_PATH
 from .common import SRC_PATH
 
-with open(os.path.join(SRC_PATH, "configs/ssd-mobilenet-v1-fpn/template.config"), 'r') as f:
+MODEL_NAME = 'ssd-resnet50-v1-fpn'
+
+with open(os.path.join(SRC_PATH, "configs/%s/template.config" % MODEL_NAME), 'r') as f:
   config = f.readlines()
 
-with open(os.path.join(SRC_PATH, "configs/ssd-mobilenet-v1-fpn/train.config"), 'w') as f:
+with open(os.path.join(SRC_PATH, "configs/%s/train.config" % MODEL_NAME), 'w') as f:
   for line in config:
     if '$PROJECT_PATH' in line:
       line = line.replace('$PROJECT_PATH', PROJECT_PATH)
