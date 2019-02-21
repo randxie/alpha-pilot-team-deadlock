1d5db982a14b2caa87d2c37a137318f376cbea72
randxie <randxiexyy29@gmail.com>
2019-02-21 01:28:34 -0800

    update scorer and submitter using official code

--- vision/final_code/submitter/generate_results.py
+++ vision/final_code/submitter/generate_results.py
@@ -5,20 +5,20 @@ import json
 import cv2
 import numpy as np
 
+# Implement a function that takes an image as an input, performs any preprocessing steps and outputs a list of bounding box detections and assosciated confidence score. 
 
-# Implement a function that takes an image as an input, performs any preprocessing steps and outputs a list of bounding box detections and assosciated confidence score.
 
-
-class GenerateFinalDetections(object):
-  def __init__(self):
-    self.seed = 2018
-
-  def predict(self, img):
-    np.random.seed(self.seed)
-    n_boxes = np.random.randint(4)
-    if n_boxes > 0:
-      bb_all = 400 * np.random.uniform(size=(n_boxes, 9))
-      bb_all[:, -1] = 0.5
-    else:
-      bb_all = []
-    return bb_all.tolist()
+class GenerateFinalDetections():
+    def __init__(self):
+        self.seed = 2018
+        
+    def predict(self,img):
+        np.random.seed(self.seed)
+        n_boxes = np.random.randint(4)
+        if n_boxes>0:
+            bb_all = 400*np.random.uniform(size = (n_boxes,9))
+            bb_all[:,-1] = 0.5
+        else:
+            bb_all = []
+        return bb_all.tolist()
+        
--- vision/final_code/submitter/generate_submission.py
+++ vision/final_code/submitter/generate_submission.py
@@ -6,33 +6,35 @@ import cv2
 import numpy as np
 from random import shuffle
 
-from .generate_results import *
+from generate_results import *
 import time
+
 
 img_file = glob.glob('testing/images/*.JPG')
 img_keys = [img_i.split('/')[-1] for img_i in img_file]
 
+
+
 # Instantiate a new detector
 finalDetector = GenerateFinalDetections()
-
 # load image, convert to RGB, run model and plot detections. 
 time_all = []
 pred_dict = {}
 for img_key in img_keys:
-  img = cv2.imread('testing/images/' + img_key)
-  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  tic = time.monotonic()
-  bb_all = finalDetector.predict(img)
-  toc = time.monotonic()
-  pred_dict[img_key] = bb_all
-  time_all.append(toc - tic)
+    img =cv2.imread('testing/images/'+img_key)
+    img =cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    tic = time.monotonic()
+    bb_all = finalDetector.predict(img)
+    toc = time.monotonic()
+    pred_dict[img_key] = bb_all
+    time_all.append(toc-tic)
 
 mean_time = np.mean(time_all)
-ci_time = 1.96 * np.std(time_all)
-freq = np.round(1 / mean_time, 2)
-
-print('95% confidence interval for inference time is {0:.2f} +/- {1:.4f}.'.format(mean_time, ci_time))
+ci_time = 1.96*np.std(time_all)
+freq = np.round(1/mean_time,2)
+    
+print('95% confidence interval for inference time is {0:.2f} +/- {1:.4f}.'.format(mean_time,ci_time))
 print('Operating frequency from loading image to getting results is {0:.2f}.'.format(freq))
 
 with open('random_submission.json', 'w') as f:
-  json.dump(pred_dict, f)
+    json.dump(pred_dict, f)
