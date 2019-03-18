4fc9bb293fbf6b05bf7fab1732d51c4ea27de941
randxie <randxiexyy29@gmail.com>
2019-03-18 00:06:34 -0700

    adjust parameters

--- integration/run_state_machine_final.py
+++ integration/run_state_machine_final.py
@@ -107,9 +107,20 @@ class StateMachine(object):
       gate_center = self._planner.gate_map.get(GATE_ORDER[self._cur_gate_id], None)
       gate_vec_loc = self._planner.vec_map.get(GATE_ORDER[self._cur_gate_id], None)
       gate_loc = gate_center - gate_vec_loc * 2 * TARGET_HEADING[self._cur_gate_id] * np.sign(gate_center[1])
-      if np.linalg.norm(np.array(self._env.states[0:3]) - np.array(gate_loc)) < 0.5:
+      gate_width, gate_height = self._planner.dim_map[GATE_ORDER[self._cur_gate_id]]
+      if np.linalg.norm(np.array(self._env.states[0:3]) - np.array(gate_loc)) < (gate_width / 2):
         self._sys_state = SystemState.GATE_ADJUST_POSE
+
+      if np.linalg.norm(np.array(self._env.states[0:3]) - np.array(gate_loc)) < 6:
+        gate_loc_ref = gate_center + gate_vec_loc * 4 * TARGET_HEADING[self._cur_gate_id] * np.sign(gate_center[1])
+        desired_state_ref = self._planner.get_desired_state(self._env.states, next_gate_loc=gate_loc_ref)
+      else:
+        desired_state_ref = None
+
       desired_states = self._planner.get_desired_state(self._env.states, next_gate_loc=gate_loc)
+      if desired_state_ref is not None:
+        desired_states[5] = desired_state_ref[5]
+
     elif target_sys_state == SystemState.GATE_PASSED:
       """
       if it is close enough to gate, transit to gate passing, otherwise stay
@@ -118,7 +129,7 @@ class StateMachine(object):
       gate_vec_loc = self._planner.vec_map.get(GATE_ORDER[self._cur_gate_id], None)
       gate_loc = gate_center + 3 * gate_vec_loc * TARGET_HEADING[self._cur_gate_id] * np.sign(gate_center[1])
       desired_states = self._planner.get_desired_state(self._env.states, next_gate_loc=gate_loc)
-      if np.linalg.norm(np.array(self._env.states[0:3]) - np.array(gate_loc)) < 1:
+      if np.linalg.norm(np.array(self._env.states[0:3]) - np.array(gate_loc)) < 1.5:
         self._planner.reset()
         self._cur_gate_id += 1
         self._sys_state = SystemState.GATE_FOUND
@@ -147,7 +158,7 @@ if __name__ == '__main__':
   state_machine = StateMachine(env, controller, planner, localizer, explorer, gate_estimator)
 
   while True:
-    rospy.sleep(0.01)
+    rospy.sleep(0.0075)
     try:
       state_machine.spin()
     except rospy.ROSInterruptException:
